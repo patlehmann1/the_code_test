544868f70a4a50a9a6716ada8fddb55f607d28d0
pat <patlehmann1@gmail.com>
2018-10-27 16:50:54 -0400

    Updates

---
+++ precache-manifest.16ead146ba4d0e2f2366251c723006af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26c7ceef0027c562d9c6",
+    "url": "/the_code_test/static/js/runtime~main.26c7ceef.js"
+  },
+  {
+    "revision": "68fd9bbafb4679d3fc87",
+    "url": "/the_code_test/static/js/main.68fd9bba.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/the_code_test/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "68fd9bbafb4679d3fc87",
+    "url": "/the_code_test/static/css/main.bf95259c.chunk.css"
+  },
+  {
+    "revision": "835813a1032311f86c5da7a1dcbdac70",
+    "url": "/the_code_test/index.html"
+  }
+];
